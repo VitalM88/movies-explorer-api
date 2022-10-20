f73365edfdde7fbc1dd0a7aabfefed7e8d07c5e7
Vitalii Matveenkov <vitalii_matveenkov@mail.ru>
2022-10-20 13:39:25 +0300

    create index.js

--- app.js
+++ app.js
@@ -2,16 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { celebrate, Joi, errors } = require('celebrate');
+const { errors } = require('celebrate');
 const cors = require('cors');
 const helmet = require('helmet');
-const auth = require('./middlewares/auth');
 const error = require('./middlewares/error');
-const { login, createUser } = require('./controllers/users');
 require('dotenv').config();
-const NotFound = require('./errors/NotFound');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { corsConfig } = require('./utils/corsConfig');
+const router = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
 
@@ -29,27 +27,7 @@ mongoose.connect('mongodb://localhost:27017/moviesdb', {
   useNewUrlParser: true,
 });
 
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), login);
-
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    email: Joi.string().required().email({ tlds: { allow: false } }),
-    password: Joi.string().required(),
-  }),
-}), createUser);
-
-app.use('/users', auth, require('./routes/users'));
-app.use('/movies', auth, require('./routes/movies'));
-
-app.use('*', (req, res, next) => {
-  next(new NotFound('Страница не найдена'));
-});
+app.use('/', router);
 
 app.use(errorLogger);
 app.use(errors());
