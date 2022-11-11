9392da77ca79ee2800f26289adf4913c68017c1c
Vitalii Matveenkov <vitalii_matveenkov@mail.ru>
2022-11-11 19:55:53 +0300

    add localhost3000

--- utils/corsConfig.js
+++ utils/corsConfig.js
@@ -2,6 +2,8 @@ module.exports.corsConfig = {
   origin: [
     'https://mesto.vitalm.nomoredomains.icu',
     'http://mesto.vitalm.nomoredomains.icu',
+    'https://localhost:3000',
+    'http://localhost:3000',
   ],
   credentials: true,
 };
