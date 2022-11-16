f5745bb1f1626a599d66628d1384b779580e7c87
Vitalii Matveenkov <vitalii_matveenkov@mail.ru>
2022-11-16 18:01:04 +0300

    edit cors

--- utils/corsConfig.js
+++ utils/corsConfig.js
@@ -1,7 +1,7 @@
 module.exports.corsConfig = {
   origin: [
-    'https://mesto.vitalm.nomoredomains.icu',
-    'http://mesto.vitalm.nomoredomains.icu',
+    'https://movie.vitalm.nomoredomains.icu',
+    'http://movie.vitalm.nomoredomains.icu',
     'https://localhost:3000',
     'http://localhost:3000',
   ],
